57764c430da18ade4536a35be777868c3cda47f8
NICK NAME <misha.shick@gmail.com>
2022-08-02 15:16:39 +0300

    ok

--- list.c
+++ list.c
@@ -1,29 +1,167 @@
 #include "list.h";
+#include <stddef.h>
+
 int main()
 {
-    return 1;
+    listPtr_s list = init_node();
 }
-// add a node to beginning of the list
-int add(char* t)
+// call malloc and return the pointer. in case of memory allocation failure return NULL;
+inline static listPtr_s init_node()
 {
-    
+    return (listPtr_s)malloc(sizeof(list_s));
 }
-//add a node to the end of the list
-int append(listPtr_s t);
-//add a node to the specified index
-int add_at(int i);
+// add a node to the beginning of the list
+int add(listPtr_s list, listPtr_s t)
+{
+    int status = 1;
+    if (list == NULL)
+    {
+        list = t;
+    }
+    else
+    {
+        t->next_t = list;
+        list = t;
+    }
+    return status;
+}
+// add a node to the end of the list
+int append(listPtr_s list, listPtr_s t)
+{
+    int status = 1;
+    listPtr_s tmp;
+    if (list == NULL)
+    {
+        list = t;
+    }
+    else
+    {
+        tmp = list;
+        while (tmp->next_t != NULL)
+        {
+            tmp = tmp->next_t;
+        }
+        tmp->next_t = t;
+    }
+}
+// add a node to the specified index.
+// in the index is illegal return 0, otherwise return 1
+int add_at(listPtr_s list, listPtr_s t, int i)
+{
+    int status = 1;
+    int counter = 0;
+    listPtr_s tmp = list;
+    if (i < 0 || i >= list_size(list))
+    {
+        status = 0;
+    }
 
-//all the remove functions return the node that was removed, or NULL if it was not found
-//remove a node from the end of the list
-listPtr_s remove(listPtr_s t);
-//remove a node from the beginning of the list
-listPtr_s remove_first(listPtr_s t);
-//remove a node from the specified index
-listPtr_s remove_at(int i);
+    while (tmp->next_t != NULL || counter != i)
+    {
+        tmp = tmp->next_t;
+        ++counter;
+    }
+}
+// all the remove functions return a copy of the node that was removed, or NULL if it was not found
+// all the remove functions must free the removed node
+// remove a node from the beginning of the list
+listPtr_s remove(listPtr_s list_s, listPtr_s t)
+{
+    listPtr_s res = NULL;
+    if (list_s != NULL)
+    {
+        res = list_s->next_t;
+        res->next_t = NULL;
+        list_s = list_s->next_t;
+    }
+    return res;
+}
+// remove a node from the end of the list
+listPtr_s remove_last(listPtr_s list_s, listPtr_s t)
+{
+    listPtr_s tmp = list_s;
+    listPtr_s res = NULL;
+    int counter = 0;
+    if (list_s != NULL)
+    {
+        while (tmp->next_t != NULL)
+        {
+            if (tmp->next_t->next_t == NULL)
+            {
+                res = tmp->next_t;
+                tmp->next_t = NULL;
+                break;
+            }
+            tmp = tmp->next_t;
+            ++counter;
+        }
+    }
+}
+// remove a node from the specified index
+listPtr_s remove_at(listPtr_s list_s, int i)
+{
+    listPtr_s tmp = list_s;
+    listPtr_s res = NULL;
+    int counter = 0;
+    if (list_s != NULL)
+    {
+        while (tmp->next_t != NULL || counter != i)
+        {
+            tmp = tmp->next_t;
+            ++counter;
+        }
+    }
+}
 
-//check if the list is empty
-int is_empty(listPtr_s t);
-//check if the list contains the given node.
-// return the node position in the list or -1 if it isn't present;
-int has_node(listPtr_s t, char* n);
+// check if the list contains the given node.
+//  return the node position in the list or -1 if it isn't present;
+int has_node(listPtr_s list, listPtr_s t)
+{
+    listPtr_s tmp = list;
+    int counter = 0;
+    if (list != NULL)
+    {
+        while (tmp != t || tmp != NULL)
+        {
+            ++counter;
+            tmp = tmp->next_t;
+        }
+        if (tmp == NULL)
+        {
+            counter = -1;
+        }
+    }
+    return counter;
+}
+// print all the nodes in the list
+void print_list(listPtr_s t)
+{
+    int counter = 0;
+    while (t)
+    {
+        printf("%d. %s - ", counter, t->msg_t, t->status);
+    }
+}
+// get the list size
+int list_size(listPtr_s t)
+{
+    int counter = 0;
+    while (t)
+    {
+        counter++;
+    }
+    return counter;
+}
 
+// free all the nodes in the list
+void free_list(listPtr_s t)
+{
+    listPtr_s tmp1 = t;
+    listPtr_s tmp2;
+    while (tmp1)
+    {
+        tmp2 = tmp1;
+        tmp1 = tmp1->next_t; 
+        free(tmp2);
+    }
+}
--- list.h
+++ list.h
@@ -1,3 +1,5 @@
+
+
 #define msg_len 256
 #define status_len 64
 typedef struct node{
@@ -6,33 +8,35 @@ typedef struct node{
     struct node *next_t;
 }list_s,*listPtr_s;
 //call malloc and return the pointer. in case of memory allocation failure return NULL;
-listPtr_s init();
+listPtr_s init_node();
 
 //all the add functions return 1 on success, 0 on failure;
 //add a node to the beginning of the list
-int add(char* t);
+int add(listPtr_s list_s,listPtr_s t);
 //add a node to the end of the list
-int append(listPtr_s t);
+int append(listPtr_s list_s,listPtr_s t);
 //add a node to the specified index
-int add_at(int i);
+int add_at(listPtr_s list_s,listPtr_s t, int i);
 
 //all the remove functions return the node that was removed, or NULL if it was not found
-//remove a node from the end of the list
-listPtr_s remove(listPtr_s t);
 //remove a node from the beginning of the list
-listPtr_s remove_first(listPtr_s t);
+listPtr_s remove(listPtr_s list_s, listPtr_s t);
+//remove a node from the end of the list
+listPtr_s remove_last(listPtr_s list_s,listPtr_s t);
 //remove a node from the specified index
-listPtr_s remove_at(int i);
+listPtr_s remove_at(listPtr_s list_s,int i);
 
-//replace the specified node with a new node.
-// if the replacement target isn't in the list return -1
-// otherwise return the node index;
-//check if the list is empty
-int is_empty(listPtr_s t);
 //check if the list contains the given node.
 // return the node position in the list or -1 if it isn't present;
-int has_node(listPtr_s t, char* n);
+int has_node(listPtr_s list, listPtr_s t)
+;
 //print all the nodes in the list
 void print_list(listPtr_s t);
+
+//get the list size
+int list_size(listPtr_s t);
+
+//free all the nodes in the list
+void free_list(listPtr_s t);
 
 
